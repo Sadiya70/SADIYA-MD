eec554e37b96ffb02afc28723bb3f044f867d1de
Sadiya70 <sadiyaofc44@gmail.com>
2024-10-23 20:34:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", // මෙතනට SESSION_ID එක දෙන්න
-MONGODB: process.env.MONGODB || "", // mongodb url එක දෙන්න
+SESSION_ID: process.env.SESSION_ID || "qbRwUQRY#jyeQmlSA2dQtF8mMzghPl_9eyAtrPb2J-5v9eB3ycmM", // මෙතනට SESSION_ID එක දෙන්න
+MONGODB: process.env.MONGODB || "mongodb://mongo:lHEiZPmFVdVYfLXPuuIlStvrkXIhWJYF@autorack.proxy.rlwy.net:20671", // mongodb url එක දෙන්න
 };
